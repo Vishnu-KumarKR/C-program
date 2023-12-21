ed710003061c42205e2e47a3aeb74c37631ff2fc
Vikram Ganesh <111498956+VikramGanesh06@users.noreply.github.com>
2023-12-21 11:45:38 +0530

    Update Swapping.c

--- Swapping.c
+++ Swapping.c
@@ -5,9 +5,9 @@ int main(){
 	scanf("%d",num1);
 	printf("Enter the Second Number:");
 	scanf("%d",num2);
-	num1=num1+num2;
-	num2=num1-num2;
-	num1 = num1-num2;
+	num1 = num1 + num2;
+	num2 = num1 - num2;
+	num1 = num1 - num2;
 	printf("After swapping, num1= %d, num2=%d\n",num1,num2);
 	return 0;
 }
